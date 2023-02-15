6af016abbca7184cacbc7513d8e3582d89c39803
Echo chen <90811429+echodpp@users.noreply.github.com>
2023-02-15 19:43:51 +0000

    add new project

---
+++ pngme/target/debug/deps/pngme-66b8d53a0fe28321.d
@@ -0,0 +1,5 @@
+/workspaces/Echo_rust_repo/pngme/target/debug/deps/pngme-66b8d53a0fe28321: src/main.rs
+
+/workspaces/Echo_rust_repo/pngme/target/debug/deps/pngme-66b8d53a0fe28321.d: src/main.rs
+
+src/main.rs:
---
+++ pngme/target/debug/pngme.d
@@ -0,0 +1,1 @@
+/workspaces/Echo_rust_repo/pngme/target/debug/pngme: /workspaces/Echo_rust_repo/pngme/src/main.rs
