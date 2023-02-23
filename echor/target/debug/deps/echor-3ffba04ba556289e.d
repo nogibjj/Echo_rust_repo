206bd729322f6b57af65e1993071d36c1e6d3da8
Echo chen <90811429+echodpp@users.noreply.github.com>
2023-02-23 04:57:28 +0000

    add

---
+++ echor/target/debug/deps/echor-3ffba04ba556289e.d
@@ -0,0 +1,5 @@
+/workspaces/Echo_rust_repo/echor/target/debug/deps/echor-3ffba04ba556289e: src/main.rs
+
+/workspaces/Echo_rust_repo/echor/target/debug/deps/echor-3ffba04ba556289e.d: src/main.rs
+
+src/main.rs:
---
+++ echor/target/debug/echor.d
@@ -0,0 +1,1 @@
+/workspaces/Echo_rust_repo/echor/target/debug/echor: /workspaces/Echo_rust_repo/echor/src/main.rs
