af26d878d296d9000b208017c2cba7e2b1ca7056
Echo chen <90811429+echodpp@users.noreply.github.com>
2023-03-02 04:34:15 +0000

    update

---
+++ pngme/README.md
@@ -0,0 +1,53 @@
+![Screen Shot 2023-02-15 at 2 36 58 PM](https://user-images.githubusercontent.com/90811429/219137450-32d34467-90ce-44e7-8cd3-f85c84a81c3f.png)
+
+
+I want to master the ability to use rust to become, so I plan to build a rust intermediate project step by step, following the tests and ideas provided in this textbook
+https://picklenerd.github.io/pngme_book/introduction.html
+
+## Introduction 
+
+We're making a command line program that lets you hide secret messages in PNG files. Your program will have four commands:
+
+- Encode a message into a PNG file
+- Decode a message stored in a PNG file
+- Remove a message from a PNG file
+- Print a list of PNG chunks that can be searched for messages 
+- 
+If that sounds scary and beyond your ability then this guide is definitely for you. If you know how to write code and you know your Rust basics, you can totally do this. We're not going to implement any sort of image decoding. The part of the PNG spec we're tackling is surprisingly simple.  
+
+The small project is based on the creation and parsing of a custom Png. 
+The PNG file structure is actually very simple, consisting of a fixed header signature and a combination of multiple chunks, as shown in the following figure
+![image.png](https://i.stack.imgur.com/hcIIO.png)
+
+There are many more details, including the definition of chunk_type, chunk_type consists of their respective letters, the difference in case represents the different characteristics of the chunk.
+
+This is my second Rust project to hide a string of information into a png image. The principle is to add a new chunk that has no effect on the display of the image, and parse the chunk to get the hidden information.
+Reference link. [https://picklenerd.github.io/pngme_book/introduction.html](https://picklenerd.github.io/pngme_book/introduction.html)
+
+# Data Structur
+## ChunkType
+```rust
+pub struct ChunkType(u8, u8, u8, u8);
+```
+## Chunk
+```rust
+pub struct Chunk {
+    data_length: u32,
+    chunk_type: ChunkType,
+    data: Vec<u8>,
+    crc: u32,
+}
+```
+## Png
+```rust
+pub struct Png {
+    signature: [u8; 8],
+    pub(crate) chunks: Vec<Chunk>,
+}
+```
+# Example
+pngme encode <file_path> <chunk_type> <message> <out_file_path>
+pngme decode <file_path> <chunk_type>
+pngme remove <file_path> <chunk_type>
+pngme print <file_path>
+
--- pngme/src/main.rs
+++ pngme/src/main.rs
@@ -1,12 +1,72 @@
+use std::fs;
+use std::str::FromStr;
+
+use clap::Parser;
+
+use crate::chunk::Chunk;
+use crate::chunk_type::ChunkType;
+use crate::error::PngError;
+use crate::png::Png;
+
 mod args;
 mod chunk;
 mod chunk_type;
 mod commands;
 mod png;
+mod error;
+mod utils;
 
-pub type Error = Box<dyn std::error::Error>;
-pub type Result<T> = std::result::Result<T, Error>;
+// cargo run
+fn main() -> Result<(), PngError> {
+    let cli = args::Cli::parse();
 
-fn main() -> Result<()> {
-    todo!()
+    match &cli.command {
+        args::Command::Encode(cmd) => {
+            // get basic info
+            let input_file = &cmd.file_path;
+            let chunk_type = ChunkType::from_str(cmd.chunk_type.as_str())?;
+            let chunk_data = cmd.message.clone().into_bytes();
+            let message_chunk = Chunk::new(chunk_type, chunk_data);
+            // create Png from file path
+            let mut png = Png::from_file(input_file)?;
+            // add secret message chunk
+            png.append_chunk(message_chunk);
+            // output new file path
+            if let Some(output_path) = &cmd.out_path {
+                fs::write(output_path, png.as_bytes())?;
+            }
+        }
+        args::Command::Decode(cmd) => {
+            let input_file = &cmd.file_path;
+            let chunk_type = cmd.chunk_type.as_str();
+            let png = Png::from_file(input_file)?;
+            let message = png.data_string_by_type(chunk_type);
+            match message {
+                None => { println!("no such message for chunk_type: {}.", chunk_type) }
+                Some(msg) => {
+                    println!("secret msg for {} is: {}", chunk_type, msg)
+                }
+            }
+        }
+        args::Command::Remove(cmd) => {
+            let input_file = &cmd.file_path;
+            let chunk_type = cmd.chunk_type.as_str();
+            let mut png = Png::from_file(input_file)?;
+            let chunk_removed = png.remove_chunk(chunk_type)?;
+            fs::write(input_file, png.as_bytes())?;
+            println!("remove chunk type: {}", chunk_type);
+        }
+        args::Command::Print(cmd) => {
+            let input_file = &cmd.file_path;
+            let mut png = Png::from_file(input_file)?;
+            let v = &png.chunks;
+            let num = v.len();
+            println!("====================all chunk type({num})====================");
+            for i in 0..num {
+                print!("{}:{};", i + 1, v[i].chunk_type().to_string());
+            }
+        }
+    }
+
+    Ok(())
 }
